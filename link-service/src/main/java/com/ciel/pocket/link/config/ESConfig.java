858d6c79323fe01b7f316614edb12c5b0952dff9
CielQian <qianhong91@outlook.com>
2019-05-07 07:06:25 +0800

    添加es

---
+++ link-service/src/main/java/com/ciel/pocket/link/config/ESConfig.java
@@ -0,0 +1,13 @@
+package com.ciel.pocket.link.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author cielqian
+ * @Email qianhong91@outlook.com
+ * @CreateTime 2019-05-07 05:28
+ * @Comment
+ */
+@Configuration
+public class ESConfig {
+}
