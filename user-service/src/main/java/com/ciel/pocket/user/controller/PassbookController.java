762c9d97c1b8077af02af7007821f85fd92b47f3
CielQian <qianhong91@outlook.com>
2019-05-30 15:39:01 +0800

    passbook更新、删除

--- user-service/src/main/java/com/ciel/pocket/user/controller/PassbookController.java
+++ user-service/src/main/java/com/ciel/pocket/user/controller/PassbookController.java
@@ -1,26 +1,22 @@
 package com.ciel.pocket.user.controller;
 
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.ciel.pocket.infrastructure.constants.Constants;
 import com.ciel.pocket.infrastructure.dto.web.PageOutput;
 import com.ciel.pocket.infrastructure.dto.web.ReturnModel;
 import com.ciel.pocket.infrastructure.utils.ReturnUtils;
 import com.ciel.pocket.user.domain.Passbook;
-import com.ciel.pocket.user.domain.Tip;
 import com.ciel.pocket.user.service.PassbookService;
-import com.netflix.discovery.util.StringUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 /**
  * @author cielqian
@@ -37,7 +33,8 @@ public class PassbookController {
 
     @ApiOperation("创建账号")
     @RequestMapping(method = RequestMethod.POST)
-    public ReturnModel<Passbook> create(@RequestBody @Valid Passbook passbook, BindingResult bindingResult){
+    public ReturnModel<Passbook> create(@RequestBody @Valid Passbook passbook, @RequestHeader(Constants.Header_AccountId) Long accountId){
+        passbook.setUserId(accountId);
         passbookService.save(passbook);
         log.info("create passbook");
         passbook.setPassword("");
@@ -46,8 +43,10 @@ public class PassbookController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ReturnModel<PageOutput<Passbook>> getAll(Page page
+            ,@RequestHeader(Constants.Header_AccountId) Long accountId
             ,@RequestParam(value = "note", required = false) String note){
         QueryWrapper<Passbook> qw = new QueryWrapper<Passbook>();
+        qw.eq("user_id", accountId);
         if (StringUtils.isNotEmpty(note)){
             qw.like("note", note);
         }
@@ -64,5 +63,13 @@ public class PassbookController {
         passbookService.removeById(id);
         log.info("delete passbook id " + id);
         return ReturnUtils.ok("删除成功",id);
+    }
+
+    @ApiOperation("更新账号")
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ReturnModel<Long> update(@PathVariable("id") Long id, @RequestBody @Valid Passbook passbook){
+        passbookService.updateById(passbook);
+        log.info("update passbook id " + id);
+        return ReturnUtils.ok("更新成功",id);
     }
 }
