5006c42a152e665e4962cc87d83ee18158a7cb34
CielQian <qianhong91@outlook.com>
2019-06-27 22:21:28 +0800

    添加websocket

--- gateway/src/main/java/com/ciel/loadstar/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/ciel/loadstar/gateway/GatewayApplication.java
@@ -1,17 +1,21 @@
 package com.ciel.loadstar.gateway;
 
+import com.github.mthizo247.cloud.netflix.zuul.web.socket.EnableZuulWebSocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableSwagger2
 @EnableResourceServer
+@EnableZuulWebSocket
+@EnableWebSocketMessageBroker
 public class GatewayApplication extends ResourceServerConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
