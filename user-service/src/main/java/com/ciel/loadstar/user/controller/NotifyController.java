711436db7193761901cda24b7f77b8f5c5742379
CielQian <qianhong91@outlook.com>
2019-07-01 17:38:48 +0800

    调整项目结构

---
+++ user-service/src/main/java/com/ciel/loadstar/user/controller/NotifyController.java
@@ -0,0 +1,13 @@
+package com.ciel.loadstar.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author cielqian
+ * @email qianhong91@outlook.com
+ * @date 2019/6/28 9:32
+ */
+
+@RestController
+public class NotifyController {
+}
